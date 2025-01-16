678effd444fa56d384d8f30af4fc98aa2812402f
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-01-16 23:16:17 +0000

    Update RandomPasswordGenerator.java

--- Random Password Generator/RandomPasswordGenerator.java
+++ Random Password Generator/RandomPasswordGenerator.java
@@ -1,5 +1,5 @@
 /**This java program is designed to generates and validates
- * passwords based on specified criteria: minimum length, minimum numbers of uppercase letters,
+ * passwords based on specified criteria: minimum length, minimum numbers of uppercase letters, 
  * digits, and special characters, and a set of allowed special characters.
  *  The program consists of two methods: 'generatePassword' for creating passwords
  *  and 'checkPassword' for validating them, along with a 'main' method for user interaction.
