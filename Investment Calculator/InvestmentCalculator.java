1346cbd6b0eff330ae7226f5b38429ea742a3b87
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-02-20 23:45:53 +0000

    Update InvestmentCalculator.java

--- Investment Calculator/InvestmentCalculator.java
+++ Investment Calculator/InvestmentCalculator.java
@@ -1,4 +1,4 @@
-                          
+                           
 /** This java program compute the total value of an investment and the interest earned over a period. It prompts the user:
 -For initial amount to invest (principal)
 -The yearly rate (rate)
